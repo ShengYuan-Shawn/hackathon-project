7f1a5fc9ba2bc43ee0d5c183b4633de9d6cb7a7c
Shawn Lim <lsy2pw@gmail.com>
2022-07-19 10:33:06 +0800

    first commit

---
+++ src/DataFetching.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// const BaseURL = "https://api.opendota.com/api";
+const heroBaseURL = "https://api.opendota.com/api/heroes";
+// const heroesStateURL = "https://api.opendota.com/api/heroStats";
+
+function DataFetching() {
+  const [posts, setPosts] = React.useState([]);
+
+  useEffect(() => {
+    axios
+      .get(heroBaseURL)
+      .then((respond) => {
+        console.log(respond);
+        setPosts(respond.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ol>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.localized_name}, {post.attack_type}, {post.roles}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default DataFetching;
