34dd1679efc0cb9623b9eab516f9dfa5bfff63be
Shawn Lim <lsy2pw@gmail.com>
2022-07-20 14:48:47 +0800

    shit

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -1,13 +1,44 @@
 import React from "react";
+import illustration from "../assets/dev.jpg";
 
 const About = () => {
   return (
-    <footer>
-      <div className="footer-container">
-        <div></div>
-        <p> 2022 FE Team 4A - SuperoCo| ALl Right Reserved.</p>
+    <section>
+      <div className="about-container">
+        <div className="about-content">
+          <div className="about-header">
+            <p>
+              About Us
+              <hr />
+            </p>
+          </div>
+
+          <div className="about-content-left">
+            <img
+              src={illustration}
+              className="dev-illustration"
+              alt="dev-illustration"
+            />
+          </div>
+
+          <div className="about-content-middle">
+            <br />
+            <h4>More About Us</h4>
+            <p>
+              We're a group of fresh graduate that came from different study
+              fields, and we have try to create an web application, SuperoCo
+              that allow users to view and search superhero description and
+              details. We have try to create a clean and responsive web design
+              in SuperoCo.
+            </p>
+            <br />
+            <br />
+            <h4>Developers Wannabe -</h4>
+            <p>Amir Bin Ahmad, Farah Afiqah, Lim Sheng Yuan</p>
+          </div>
+        </div>
       </div>
-    </footer>
+    </section>
   );
 };
 
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,27 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
     <footer>
       <div className="footer-container">
-        <div className="about-content">
-          <h3>About Us</h3>
-          <p>
-            We're a group of fresh graduate's developer wannabe <br />
-            trying todeveloper a superhero codex app using REST API.
-          </p>
-        </div>
-        <div className="nav-menu">
+        <div className="about-desc">
+          <p>About Us</p>
           <ul>
-            <li>Home</li>
-            <li>Heroes</li>
-            <li>Forum</li>
-            <li>About</li>
+            <li OnClick={() => navigate("/about")}>Who we are</li>
+          </ul>
+        </div>
+
+        <div className="contact-details">
+          <p>Support</p>
+          <ul>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+
+        <div className="social-media">
+          <p>Social Media</p>
+          <ul>
+            <li></li>
           </ul>
         </div>
       </div>
       <div className="copyright-content">
-        <p> 2022 FE Team 4A - SuperoCo | ALL Right Reserved.</p>
+        <p>All Right Reserved 2022 © by FE Team 4A</p>
       </div>
     </footer>
   );
