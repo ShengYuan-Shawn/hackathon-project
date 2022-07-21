3e76bcb69eedbd3d51af195fa6e40e43f3572e73
Shawn Lim <lsy2pw@gmail.com>
2022-07-21 17:44:47 +0800

    major changes

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,17 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import stars from "../assets/stars.png";
+import city from "../assets/city.png";
+import men from "../assets/men.png";
+import women from "../assets/women.png";
 
 const Home = () => {
+  //reference to the start
+  const starRef = React.useRef();
+  const menRef = React.useRef();
+  const womenRef = React.useRef();
+
+  React.useEffect(() => {
+    if (!starRef.current || !menRef.current || !womenRef.current) return;
+    function ___() {
+      let value = window.scrollY;
+      starRef.current.style.top = `${value * 0.75}px`;
+      menRef.current.style.left = `${value * 1.32}px`;
+      womenRef.current.style.left = `${value * 0.75}px`;
+    }
+    window.addEventListener("scroll", ___);
+    return () => window.removeEventListener("scroll", ___);
+  }, [starRef, menRef, womenRef]);
+
   return (
     <div className="home-container">
+      <section className="animation">
+        <img src={stars} id="stars" ref={starRef} alt="stars" />
+        <img src={city} id="city" alt="city" />
+        <img src={men} id="men" ref={menRef} alt="men" />
+        <img src={women} id="women" ref={womenRef} alt="women" />
+      </section>
+      <div className="scroll-down">
+        <h3>Scroll Down</h3>
+      </div>
       <div className="landing-content">
         <h2>
           Everyone Has Their Own Superhero Inside Them Waiting To Be Discovered
         </h2>
         <p>
-          Welcome to SuperoCo - We target to create a superhero codex that
-          allows users to find their favourite superhero characters (Heroes,
-          Anti-Heroes or Villians) details and their backstory with ease.
+          Welcome to SuperoCo - where you can find your favourite superhero
+          characters (Heroes, Anti-Heroes or Villians) details with ease.
         </p>
         <Link to={"/heroes"}>
           <button>Checkout A Superhero!</button>
